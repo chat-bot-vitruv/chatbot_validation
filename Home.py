c922e57ac38d01904e7e168dd1968f4f1efe7f99
bluewow <dogseen@gmail.com>
2023-12-22 13:49:06 +0900

    사이트 추가 #2

--- Home.py
+++ Home.py
@@ -11,24 +11,13 @@ sidebar.header('문제/해설 입력')
 question_input = sidebar.text_area('문제 입력')
 answer_input = sidebar.text_area('해설 입력')
 
-
-# 상단의 단일 텍스트 박스 추가
-text_box_top = st.text_area("Prompt 입력 데이터(전처리", "", height=100)
-
 # HTML/CSS를 사용하여 테두리가 있는 텍스트 박스 생성
 st.markdown('<style>div.stTextInput>div{border:2px solid #4CAF50;}</style>', unsafe_allow_html=True)
 
-# 두 개의 열 생성
-col1, col2 = st.columns(2)
-
-# 첫 번째 열에 테두리가 있는 텍스트 박스 추가
-with col1:
-    text_box1 = st.text_area("결과 1 raw data", "", height=100)
-
-# 두 번째 열에 테두리가 있는 텍스트 박스 추가
-with col2:
-    text_box2 = st.text_area("결과 2 latex", "", height=100)
-
+# 상단의 단일 텍스트 박스 추가
+text_box_1 = st.text_area("Prompt 입력 데이터(전처리", "", height=100)
+text_box_2 = st.text_area("결과 1 raw data", "", height=100)
+text_box_3 = st.text_area("결과 2 latex", "", height=100)
 
 # 사이드바에 버튼 추가
 if sidebar.button('버튼 클릭'):
