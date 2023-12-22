97ada5815a44ebd8a665fa4e10fb41384ff8b0a0
hbkim <hobeen.kim@vitruv.co.kr>
2023-12-22 11:23:16 +0900

    init setting

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Vitruv Chatbot",
+    page_icon="😀",
+)
+
+st.markdown(
+    """
+# Hello!
+            
+Vitruv Chatbot
+"""
+)
---
+++ pages/01_hbk.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="hbk",
+)
+
+def main():
+
+    st.title("hbk chatbot")
+
+if __name__ == '__main__':
+    main()
